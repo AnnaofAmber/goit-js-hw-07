319c6ce1f7fa0712917124f15d11ac0e6a39b7d6
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-07-24 12:25:35 +0300

    додає перше завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery')
+const markup = listMarkup(list);
+
+function listMarkup() {
+  const gallery = galleryItems
+  .map((img) => `<li class='gallery__item'><a class="gallery__link" href="${img.original}"><img class = 'gallery__image' src=${img.preview} alt=${img.description} data-source=${img.original}></img></a></li>`)
+  .join('');
+
+list.insertAdjacentHTML('afterbegin', gallery)}
+list.addEventListener('click', onClick)
+
+
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG')
+    return
+  const originalImg = event.target.dataset.source
+  const instance = basicLightbox.create(
+    `<div class='gallery-modal'>
+      <img src = '${originalImg}' width = '960'/>
+    </div>`
+    
+  )
+    instance.show()
+  list.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  })
+}
+
+
+
