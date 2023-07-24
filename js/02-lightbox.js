97bbfb318587834ed1e4a98240ffcfc432e711d1
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-07-24 12:53:46 +0300

    додає друге завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery')
+const markup = listMarkup(list);
+
+function listMarkup() {
+  const gallery = galleryItems
+  .map((img) => `<li class='gallery__item'><a class="gallery__link" href="${img.original}"><img class = 'gallery__image' src=${img.preview} alt=${img.description}></img></a></li>`)
+  .join('');
+
+    list.insertAdjacentHTML('afterbegin', gallery)
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+})
+
+
+
